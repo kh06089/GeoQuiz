8e1c45fc64471e58a16cfa60ad5d7124bdd8826c
kh06089 <_Musicismylif395>
2019-01-30 10:26:55 -0500

    Added next and prev buttons

---
+++ app/src/main/java/com/example/geoquiz/Question.java
@@ -0,0 +1,28 @@
+package com.example.geoquiz;
+
+public class Question {
+    private int mTextResID;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResID=textResId;
+        mAnswerTrue=answerTrue;
+    }
+
+    public int getTextResID() {
+        return mTextResID;
+    }
+
+    public void setTextResID(int textResID) {
+        mTextResID = textResID;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
+
